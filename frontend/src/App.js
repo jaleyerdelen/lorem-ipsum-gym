ee7e69d6d45f17e77f70af6b90a43918b57f6e10
jaleyerdelen <jaleyerdelen@gmail.com>
2022-03-11 19:00:54 +0300

    listed allcourses

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,59 @@
 import axios from "axios";
-import Axios from "axios";
 import React, { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Category from "./components/Category";
 
 function Home() {
-  axios
-    .get("/http://localhost:5000/courses")
-    .then((res) => {
-      console.log(res.data.courses);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-  return <h2>hello</h2>;
+  return <h2>jale's page</h2>;
 }
 
 function App() {
-  Axios({
-    method: "GET",
-    url: "http://localhost:5000/",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  }).then((res) => {
-    console.log(res.data.message);
-  });
+  const [courses, setCourses] = useState([""]);
+
+  useEffect(() => {
+    const allCourse = () => {
+      axios
+        .get("http://localhost:5000/courses")
+        .then((res) => setCourses(res.data.courses))
+        .catch((err) => console.log(err));
+    };
+
+    allCourse();
+  }, []);
+
+  const Cat = () => {
+    axios
+      .get("http://localhost:5000/category")
+      .then((res) => {
+        console.log(res.data.courses);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    return <h2>hello</h2>;
+  };
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="category" element={<Category />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <div>
+        {courses.map((course) => (
+          <div>
+            {course.name}
+            <h2>{course.description}</h2>
+          </div>
+        ))}
+      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/home" element={<Home />} />
+          <Route
+            path="category"
+            element={<Category name="world" Cat={Cat} />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
--- frontend/src/components/Category.js
+++ frontend/src/components/Category.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React from "react";
 
-const Category = ({name}) => {
-return(
+const Category = ({ name, Cat }) => {
+  return (
     <div>
-        <h2>jale</h2>
-        <h1>{name}</h1>
+      <h2>jale</h2>
+      <h1>{name}</h1>
+      <button type="button" onClick={() => Cat()}>
+        jale
+      </button>
     </div>
-)
-}
+  );
+};
 
-export default Category
+export default Category;
