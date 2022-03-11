16fb68a0a745c079f9a41e8356140307d24b8abb
jaleyerdelen <jaleyerdelen@gmail.com>
2022-03-11 08:10:48 +0300

    courses listed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,40 @@
-import React from "react";
+import axios from "axios";
 import Axios from "axios";
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Category from "./components/Category";
+
+function Home() {
+  axios
+    .get("/http://localhost:5000/courses")
+    .then((res) => {
+      console.log(res.data.courses);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  return <h2>hello</h2>;
+}
 
 function App() {
- Axios({
+  Axios({
     method: "GET",
     url: "http://localhost:5000/",
-     headers: {
-       "Content-Type": "application/json"
-     }
-  }).then(res => {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((res) => {
     console.log(res.data.message);
   });
 
   return (
-   <div>welcome</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="category" element={<Category />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/components/Category.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Category = ({name}) => {
+return(
+    <div>
+        <h2>jale</h2>
+        <h1>{name}</h1>
+    </div>
+)
+}
+
+export default Category
