c132d0bc75130be6d02b76b0c5d7ab145e492a1e
jaleyerdelen <jaleyerdelen@gmail.com>
2022-05-13 15:19:58 +0300

    category delete button

--- frontend/src/components/Category.js
+++ frontend/src/components/Category.js
@@ -5,14 +5,30 @@ import cookie from "react-cookies";
 
 const Category = () => {
   const [category, setCategory] = useState([""]);
+  const [isLoggin, setIsLogin] = useState(false);
 
   const axe = () => {
     const token = cookie.load("token");
+    if (token) {
+      setIsLogin(true);
+    } else {
+      setIsLogin(false);
+    }
     axios
       .get("http://localhost:5000/category", {
         headers: { authorization: `Baerer ${token}` },
       })
       .then((res) => setCategory(res.data.category))
+      .catch((error) => console.log(error));
+  };
+
+  const deleteCategory = (categor) => {
+    const token = cookie.load("token");
+    axios
+      .delete(`http://localhost:5000/category/${categor._id}`, {
+        headers: { authorization: `Baerer ${token}` },
+      })
+      .then((res) => console.log(res))
       .catch((error) => console.log(error));
   };
 
@@ -27,8 +43,16 @@ const Category = () => {
           <div>
             <Link to={`/category/${categor.slug}`}>
               <h1>{categor.name}</h1>
-              {/* <h2>{categor.description}</h2> */}
             </Link>
+            {isLoggin === true && (
+              <button
+                onClick={() => deleteCategory(categor)}
+                type="button"
+                className="btn btn-primary"
+              >
+                delete
+              </button>
+            )}
           </div>
         );
       })}
