2428e2dd72d2e92f8e23721f429b37a3f2804d1a
jaleyerdelen <jaleyerdelen@gmail.com>
2022-06-07 01:17:11 +0300

    fixed

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 
-const jwt = require("jsonwebtoken");
-const JWT_SECRET = process.env.JWT_SECRET;
 require("dotenv").config();
 
 const courseRoute = require("./routes/courseRoute");
@@ -11,14 +10,11 @@ const categoryRoute = require("./routes/categoryRoute");
 const userRoute = require("./routes/userRoute");
 const authRoute = require("./routes/authRoute");
 
-const authMiddleware = require("./middleware/authMiddleware")
-
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 const cors = require("cors");
-
 
 //middlewares
 app.use(express.json()); // for parsing application/json
@@ -58,19 +54,17 @@ mongoose
 
 //Routes
 app.use("/courses", courseRoute);
-app.use("/category",categoryRoute);
-app.use("/users",userRoute);
+app.use("/category", categoryRoute);
+app.use("/users", userRoute);
 app.use("/auth", authRoute);
 
-
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.status(200).json({
-    message:"success"
-  })
+    message: "success",
+  });
 });
-
-
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
